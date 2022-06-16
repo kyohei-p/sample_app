88b2dc9b3972c91d99fe0411cb9ce830bbd856b5
　”kyohei-p” <ysdkyh11@gmail.com>
2022-06-16 13:28:14 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  Set '/signup',  to: 'users#new'
+  get '/signup',  to: 'users#new'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
