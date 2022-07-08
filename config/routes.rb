e845fd001ca14d98c473dbaffacfe18c91f2203f
　”kyohei-p” <ysdkyh11@gmail.com>
2022-07-08 22:35:19 +0000

    post-delete4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
   get 'sessions/new'
-  post '/microposts/destroy'
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
@@ -11,6 +10,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  post '/microposts/:id/destroy',  to: 'microposts#destroy'
   resources :users do
     member do
       get :following, :followers
